2582c0e8ad5a99417599ec5c2ec4e2f31b746cda
Diego Krefta <diego.krefta@gmail.com>
2018-07-20 18:14:28 -0300

    Schema

--- server.js
+++ server.js
@@ -4,12 +4,15 @@ const mongoose = require("mongoose");
 
 require("dotenv").config({ path: "variables.env" });
 
+// connects to database
 mongoose
   .connect(process.env.MONGO_URI)
 
   .then(() => console.log("DB connected"))
   .catch(err => console.error(err));
 
+
+  //initial
 const app = express();
 
 const PORT = process.env.PORT || 4444;
